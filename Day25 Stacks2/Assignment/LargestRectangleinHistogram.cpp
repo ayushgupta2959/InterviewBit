38b9b50f4719c9004f155a33bb45b51e3e399187
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2019-07-14 09:40:22 +0530

    optimized solution main not added

--- Day25 Stacks2/Assignment/LargestRectangleinHistogram.cpp
+++ Day25 Stacks2/Assignment/LargestRectangleinHistogram.cpp
@@ -1,3 +1,4 @@
+/*
 int Solution::largestRectangleArea(vector<int> &A) {
     int n = A.size();
     vector<int> la(n); // to store the index of element less then A[i]
@@ -50,6 +51,45 @@ int Solution::largestRectangleArea(vector<int> &A) {
     int maxArea = -1;
     for(int i=0;i<n;++i){
         maxArea = max(maxArea,A[i]*(ra[i]-la[i]-1));
+    }
+    return maxArea;
+}
+*/
+//optimized
+int Solution::largestRectangleArea(vector<int> &A) {
+    int maxArea = 0;
+    stack<int> s;
+    int n = A.size();
+    int i = 0;
+    int left_blocker,right_blocker;
+    int top;
+    while(i<n){
+        if(s.empty()){
+            s.push(i);
+            i++;
+        }
+        else{
+            if(A[s.top()]>A[i]){
+                right_blocker = i;
+                top = A[s.top()];
+                s.pop();
+                if(!s.empty()) left_blocker = s.top();
+                else left_blocker = -1;
+                maxArea = max(maxArea,top*(right_blocker-left_blocker-1));
+            }
+            else{
+                s.push(i);
+                i++;
+            }
+        }
+    }
+    while(!s.empty()){
+        right_blocker = n;
+        top = A[s.top()];
+        s.pop();
+        if(!s.empty()) left_blocker = s.top();
+        else left_blocker = -1;
+        maxArea = max(maxArea,top*(right_blocker-left_blocker-1));
     }
     return maxArea;
 }
